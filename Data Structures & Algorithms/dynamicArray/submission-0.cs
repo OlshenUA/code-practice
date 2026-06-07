9a2a3123543e45dc24ecd3d52ab2938074785a10
Oleksii <139276388+OlshenUA@users.noreply.github.com>
2026-06-08 02:34:18 +0900

    Add: dynamicArray - submission-0

---
+++ Data Structures & Algorithms/dynamicArray/submission-0.cs
@@ -0,0 +1,52 @@
+public class DynamicArray {
+    
+    public int capacity;
+    public int[] inArray;
+    public int size;
+
+    public DynamicArray(int capacity) {
+
+        this.capacity = capacity;
+        inArray = new int [capacity];
+        size = 0;
+    }
+
+    public int Get(int i) {
+        return inArray[i];
+    }
+
+    public void Set(int i, int n) {
+        inArray[i] = n;
+    }
+
+    public void PushBack(int n) {
+
+        if (size == capacity)
+        {
+            Resize();
+        }
+
+        inArray[size] = n;
+        size ++;
+    }
+
+    public int PopBack() {
+        int lastValue = inArray[size - 1];
+        size --;
+        return lastValue;
+    }
+
+    private void Resize() {
+        int newCapacity = capacity * 2;
+        capacity = newCapacity;
+        Array.Resize(ref inArray, capacity);
+    }
+
+    public int GetSize() {
+        return size;
+    }
+
+    public int GetCapacity() {
+        return capacity;
+    }
+}
